3ac6cf99a7ec4c63c6214dda0203d44a7efed4fe
Giorgio Raccone <60217706+iorgoss95@users.noreply.github.com>
2023-08-03 10:26:16 +0200

    feat: first commit

---
+++ giorgio-raccone/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button/button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button children={"Hello first comp!"} />
+    </div>
+  );
+}
+
+export default App;
